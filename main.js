686d5e0c9077afec1145bdf2aa45c9db2c588a69
Adityacypto <84172029+Adityacypto@users.noreply.github.com>
2022-02-01 22:38:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload(){
+
+
+}
+
+
+function setup(){
+
+   canvas = createCanvas(450,450);
+  canvas.center();
+}
+
+
+function draw(){
+
+
+}
+
+function take_Snapshot(){
+
+    save("myFilterIMage.png");
+    
+}
